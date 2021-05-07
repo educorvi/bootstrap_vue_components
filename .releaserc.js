b924bb3d8f2513948175b5b20858a544951e46ac
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-07 13:40:06 +0200

    fix: Versioning

--- .releaserc.js
+++ .releaserc.js
@@ -8,7 +8,10 @@ module.exports = {
         ["@semantic-release/github", {
             "assets": ["dist/**"]
         }],
-        "@semantic-release/git"
+        ["@semantic-release/git", {
+            "assets": ["package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }]
     ],
     "preset": "angular"
 }
