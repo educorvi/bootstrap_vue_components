e1fa3d606bba763b80b8ffdfddbc75e5b2b5e454
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-07 15:05:01 +0200

    refactor: remove Hello World

--- src/components/HelloWorld/index.js
+++
@@ -1,4 +0,0 @@
-import HelloWorld from "@/components/HelloWorld/HelloWorld";
-import {options, default as Vue} from "../base";
-
-Vue.customElement('hello-world', HelloWorld, options)
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,1 @@
-import './HelloWorld'
 import './Accordion'
