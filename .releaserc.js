183e4032b8999046b56c9efbabec29e0eaa3b19f
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-07 14:32:09 +0200

    fix: Release Process

--- .releaserc.js
+++ .releaserc.js
@@ -6,12 +6,13 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         ["@semantic-release/github", {
-            "assets": ["dist/.zip"]
+            "assets": ["dist.zip"]
         }],
         ["@semantic-release/git", {
             "assets": ["package.json"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }]
+        }],
+        "@semantic-release/npm"
     ],
     "preset": "angular"
 }
