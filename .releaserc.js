42e8dc4f1b0ecaa3d77d2f8838b5f150709ef5da
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-14 11:28:12 +0200

    build: fix versioning

--- .releaserc.js
+++ .releaserc.js
@@ -3,14 +3,14 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
         ["@semantic-release/github", {
             "assets": ["dist.zip"]
         }],
         ["@semantic-release/git", {
             "assets": ["package.json"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }],
-        "@semantic-release/npm"
+        }]
     ],
     "preset": "angular"
 }
